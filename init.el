af70ec1127e33d3a4c64b312d6bd06a4ee6cd048
Yuriy Romanyshyn <yuriy.romanyshyn.lv.ua@gmail.com>
2019-11-10 22:53:34 +0200

    extend-region M-z

--- init.el
+++ init.el
@@ -96,6 +96,7 @@
 
 
 (global-set-key (kbd "M-c") `minibuffer-keyboard-quit)
+(global-set-key (kbd "M-z") `er/expand-region)
 
 
 (defhydra hydra-elpy-region-move (global-map "M-<tab>")
@@ -143,41 +144,12 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(company-quickhelp-color-background "#4F4F4F")
- '(company-quickhelp-color-foreground "#DCDCCC")
  '(custom-safe-themes
    (quote
     ("cdb4ffdecc682978da78700a461cdc77456c3a6df1c1803ae2dd55c59fa703e3" "e61752b5a3af12be08e99d076aedadd76052137560b7e684a8be2f8d2958edc3" default)))
- '(fci-rule-color "#383838")
- '(nrepl-message-colors
-   (quote
-    ("#CC9393" "#DFAF8F" "#F0DFAF" "#7F9F7F" "#BFEBBF" "#93E0E3" "#94BFF3" "#DC8CC3")))
  '(package-selected-packages
    (quote
-    (neotree elpy js2-mode swiper ivy treemacs rainbow-delimiters zenburn-theme moe-theme wakib-keys diminish use-package)))
- '(pdf-view-midnight-colors (quote ("#DCDCCC" . "#383838")))
- '(vc-annotate-background "#2B2B2B")
- '(vc-annotate-color-map
-   (quote
-    ((20 . "#BC8383")
-     (40 . "#CC9393")
-     (60 . "#DFAF8F")
-     (80 . "#D0BF8F")
-     (100 . "#E0CF9F")
-     (120 . "#F0DFAF")
-     (140 . "#5F7F5F")
-     (160 . "#7F9F7F")
-     (180 . "#8FB28F")
-     (200 . "#9FC59F")
-     (220 . "#AFD8AF")
-     (240 . "#BFEBBF")
-     (260 . "#93E0E3")
-     (280 . "#6CA0A3")
-     (300 . "#7CB8BB")
-     (320 . "#8CD0D3")
-     (340 . "#94BFF3")
-     (360 . "#DC8CC3"))))
- '(vc-annotate-very-old-color "#DC8CC3"))
+    (expand-region neotree elpy js2-mode swiper ivy treemacs rainbow-delimiters zenburn-theme moe-theme wakib-keys diminish use-package))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
